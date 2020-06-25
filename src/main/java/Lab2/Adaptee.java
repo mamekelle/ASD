297d5800ed019b2f42a34f1ad2502bf00a3ea878
mamekelle <mamekelle@gmail.com>
2020-06-25 00:07:54 -0500

    :construction: Lab2 Adapter

--- src/main/java/Lab2/Adaptee.java
+++ src/main/java/Lab2/Adaptee.java
@@ -18,7 +18,7 @@ public class Adaptee {
     }
 
     public int getEnd() {
-return end;
+    return end;
     }
 
     public String get(int pos) {
@@ -32,5 +32,28 @@ return end;
 
     public boolean empty() {
         return (end ==-1);
+    }
+    class Node  {
+        String value;
+        Node next;
+        Node previous;
+        Node(){}
+        Node(Node next, Node previous, String value){
+            this.next = next;
+            this.previous = previous;
+            this.value = value;
+        }
+        @Override
+        public String toString() {
+            if(value == null) return "";
+            StringBuilder sb = new StringBuilder(value + " ");
+            sb = toString(sb, next);
+            return sb.toString();
+        }
+        private StringBuilder toString(StringBuilder sb, Node n) {
+            if(n == null) return sb;
+            sb.append(n.value + " ");
+            return toString(sb, n.next);
+        }
     }
 }
